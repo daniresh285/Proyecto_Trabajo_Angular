dae88dd4c5f808156ca8f51990bb4f2555095b13
Daniel De Los Reyes <danieldelosreyesruiz431gmail.com>
2025-05-16 08:53:13 +0200

    Nuevos cambios en rama main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.Component.html',
-  imports: [RouterOutlet]
+  imports: [RouterOutlet, RouterModule]
 })
 export class AppComponent {
 
+  constructor(private route: ActivatedRoute) {
+    const origen = localStorage.getItem('vEngoDe');
+    console.log('Vino desde la URL del proyecto 1:', window.location.href); 
+  }
+
+  //Esto lo que hace es conectarse directamente a la URL donde esta el otro proyecto conectado 
+  //Ademas el localStorage indica que se me va enviar a la consola del navegador y lo que quiero enviarle es de donde vuelvo
   abrirEnNuevaPestana() {
-    window.open('http://localhost:4300', '_blank');
-}
+    localStorage.setItem('vEngoDe', 'proyecto 2');
+    window.location.href ='http://localhost:4300';
+  }
+  
 }
 
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const appRoutes: Routes = [
-];
+
+export const appRoutes: Routes = [];
 
