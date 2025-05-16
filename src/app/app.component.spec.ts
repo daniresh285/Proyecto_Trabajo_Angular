8af9ca4edd030b324e1b7aaddafac60abe1dbdb8
Daniel De Los Reyes <danieldelosreyesruiz431gmail.com>
2025-05-16 08:52:32 +0200

    Nuevos cambios en la página

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,6 @@ describe('AppComponent', () => {
   it(`should have the 'pagina2' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pagina2');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet],
+
 })
+
 export class AppComponent {
-  title = 'pagina2';
+
+  
+  // Cada vez que la URL tenga un parámetro llamado vengoDe
+  // Este código lo imprime en la consola. 
+  // Sirve para saber desde qué página o lugar llegó el usuario.
+  constructor(private route: ActivatedRoute) {
+    const origen = localStorage.getItem('vengoDe');
+    console.log('Vino desde la URL del proyecto 2:', window.location.href); 
+
+  }
+  volverAlProyecto1() {
+    // Esto lo que hace es que hace conexión a la anterior página y con el self lo que hace es quedarse con la página suya en vez de crear una nueva
+    localStorage.setItem('VengoDe', 'proyecto 1');
+    window.location.href = 'http://localhost:4200/';
+  }
 }
